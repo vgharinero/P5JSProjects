d74174d7d5efcb0b84c6bfa4d45a859502b36319
vgranados <100348903@alumnos.uc3m.es>
2019-09-12 18:28:25 +0200

    Added snake

---
+++ Snake/scripts/main.js
@@ -0,0 +1,109 @@
+let snake
+
+let isFood
+let foodPosition
+
+let difficulty = 0
+
+function setup()
+{
+
+    createCanvas(800, 800)
+
+    snake = new Snake(this)
+    snake.setup()
+}   
+
+function draw()
+{
+    frameRate(7 + difficulty)
+
+    background(0)
+
+    if (!isFood)
+    {
+        isFood = true
+        foodPosition = generateFood()
+    }
+
+    noStroke()
+    fill('red')
+    rect(foodPosition.X, foodPosition.Y, 40, 40)
+
+    snake.update()
+    snake.show()
+
+    if (snake.X == foodPosition.X && snake.Y == foodPosition.Y)
+    {
+        isFood = false
+        snake.eat()
+        difficulty += 0.1
+    }
+
+}
+
+function keyPressed()
+{
+    switch (key)
+    {
+        case 'ArrowUp':
+            if (snake.X != -40 && snake.X != width)
+            {
+                snake.direction = 0
+            }
+            break
+        case 'ArrowDown':
+            if (snake.X != -40 && snake.X != width)
+            {
+                snake.direction = 1
+            }
+            break
+        case 'ArrowLeft':  
+            if (snake.Y != -40 && snake.Y != height)
+            {
+                snake.direction = 2
+            }
+            break
+        case 'ArrowRight':
+            if (snake.Y != -40 && snake.Y != height)
+            {
+                snake.direction = 3
+            }
+            break
+        case ' ':
+            start = true
+    }
+}
+
+function generateFood()
+{
+    let new_point
+
+    do
+    {
+        new_point = {X: (int(random(0, 19))) * 40, Y: (int(random(0, 19))) * 40}
+    } 
+    while (inSnake(new_point))
+
+    return new_point
+}
+
+function inSnake(new_point)
+{
+    let isIn = false
+    snake.history.forEach(point => 
+        {
+            if (new_point.X == point.X && new_point.Y == point.Y)
+            {
+                isIn = true
+            }
+        }
+    )
+    return isIn
+}
+
+function restart()
+{
+    isFood = false
+    difficulty = 0
+}
---
+++ Snake/scripts/snake.js
@@ -0,0 +1,126 @@
+function Snake(main) 
+{
+
+    this.restart
+
+    this.setup = function() 
+    {
+        this.history = []
+        
+        this.direction = 1
+
+        this.body_length = 3
+
+        this.X = width/2 - 40
+        this.Y = height/2 - 40
+
+        for (let i = this.body_length; i > -1; i--)
+        {
+            this.history.push({X: this.X, Y: this.Y - (40 * i)})
+        }
+    }
+
+    this.show = function() 
+    {
+        this.restart = false
+
+        if (this.Y >= height)
+        {
+            this.Y = 0
+        }
+        if (this.Y <= -40)
+        {
+            this.Y = height - 40
+        }
+        if (this.X >= width)
+        {
+            this.X = 0
+        }
+        if (this.X <= -40)
+        {
+            this.X = width - 40
+        }
+
+        let new_point = {X: this.X, Y: this.Y}
+        this.history.forEach(point => 
+            {
+                if (new_point.X == point.X && new_point.Y == point.Y)
+                {
+                    this.restart = true
+                }
+            }
+        )
+
+        if (this.restart)
+        {
+            this.setup()
+            main.restart()
+        } 
+        else
+        {
+            this.history.push(new_point)
+
+            strokeWeight(3)
+            stroke(51)
+            fill('white')
+            rect(this.X, this.Y, 40, 40)
+
+            for (let i = 1; i < this.body_length; i++)
+            {
+                rect(this.history[this.history.length - i - 1].X, this.history[this.history.length - i - 1].Y, 40, 40)
+            }
+
+            noStroke()
+            fill('black')
+            switch (this.direction)
+            {
+                case 0:
+                    rect(this.X + 8, this.Y + 10, 6, 6)
+                    rect(this.X + 26, this.Y + 10, 6, 6)
+                    break
+                case 1:
+                    rect(this.X + 8, this.Y + 24, 6, 6)
+                    rect(this.X + 26, this.Y + 24, 6, 6)
+                    break
+                case 2:
+                    rect(this.X + 10, this.Y + 8, 6, 6)
+                    rect(this.X + 10, this.Y + 26, 6, 6)
+                    break
+                case 3:
+                    rect(this.X + 24, this.Y + 8, 6, 6)
+                    rect(this.X + 24, this.Y + 26, 6, 6)
+                    break
+            }
+    
+            while (this.history.length > this.body_length)
+            {
+                this.history.shift()
+            }
+        }
+    }
+
+    this.update = function()
+    {
+        switch (this.direction)
+        {
+            case 0:
+                this.Y -= 40
+                break
+            case 1:
+                this.Y += 40
+                break
+            case 2:
+                this.X -= 40
+                break
+            case 3:
+                this.X += 40
+                break
+        }
+    }
+
+    this.eat = function()
+    {
+        this.body_length++
+    }
+
+}
