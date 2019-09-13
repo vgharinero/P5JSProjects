907f854be7bd39d1c793dab07eca07a05986e74a
vgranados <100348903@alumnos.uc3m.es>
2019-09-13 16:29:36 +0200

    Breaker: Breaker movement

--- BrickBreaker/scripts/breaker.js
+++ BrickBreaker/scripts/breaker.js
@@ -1,4 +1,15 @@
 function Breaker()
 {
+
+    this.width = 200
+    this.height = 40
+
+    this.X = (width - this.width)/2
+    this.Y = height - this.height * 2
+
+    this.show = function()
+    {
+        rect(this.X, this.Y, this.width, this.height)
+    }
     
 }
--- BrickBreaker/scripts/main.js
+++ BrickBreaker/scripts/main.js
@@ -1,7 +1,12 @@
+let breaker
+
 function setup()
 {
 
     createCanvas(800, 800)
+
+    breaker = new Breaker()
+
 }   
 
 function draw()
@@ -9,12 +14,21 @@ function draw()
 
     background(0)
 
-}
+    breaker.show()
 
-function keyPressed()
-{
-    switch (key)
+    if (keyIsDown(LEFT_ARROW))
     {
-       
+        if (breaker.X > 0)
+        {
+            breaker.X -= 10
+        }
     }
+    else if (keyIsDown(RIGHT_ARROW))
+    {
+        if (breaker.X < (width - breaker.width))
+        {
+            breaker.X += 10
+        }
+    }
+
 }
