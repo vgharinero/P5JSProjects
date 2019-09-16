e26a9d853380de16f3ac2976eca9634a905abfcf
vgranados <100348903@alumnos.uc3m.es>
2019-09-17 00:22:58 +0200

    Breaker on progress

--- BrickBreaker/scripts/breaker.js
+++ BrickBreaker/scripts/breaker.js
@@ -7,9 +7,23 @@ function Breaker()
     this.X = (width - this.width)/2
     this.Y = height - this.height * 2
 
+    this.ball_width = 20
+
+    this.ball_X = width/2
+    this.ball_Y = height - this.height * 2 - this.ball_width * 2
+
+    this.ball_X_speed = 0
+    this.ball_Y_speed = 0
+
     this.show = function()
     {
         rect(this.X, this.Y, this.width, this.height)
+        circle(this.ball_X, this.ball_Y, this.ball_width)
+    }
+
+    this.update = function()
+    {
+        if (this.ball_X + this.ball_width/2)
     }
     
 }
